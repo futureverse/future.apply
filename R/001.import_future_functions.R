0daf486ad8fab8a5d68f9eac54c3286b910e5375
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-05-20 10:56:04 -0700

    CLEANUP: Now import all RNG-related functions from 'future'

--- R/001.import_future_functions.R
+++ R/001.import_future_functions.R
@@ -1,7 +1,16 @@
 ## To be imported from 'future', if available
 .debug <- NULL
 
+make_rng_seeds <- import_future("make_rng_seeds")
+get_random_seed <- import_future("get_random_seed")
+set_random_seed <- import_future("set_random_seed")
+next_random_seed <- import_future("next_random_seed")
+is_valid_random_seed <- import_future("is_valid_random_seed")
+is_lecyer_cmrg_seed <- import_future("is_valid_random_seed")
+as_lecyer_cmrg_seed <- import_future("as_lecyer_cmrg_seed")
+
 ## Import private functions from 'future'
 import_future_functions <- function() {
   .debug <<- import_future(".debug", mode = "environment", default = new.env(parent = emptyenv()))
+
 }
--- R/rng_utils.R
+++
@@ -1,6 +0,0 @@
-get_random_seed <- import_future("get_random_seed")
-set_random_seed <- import_future("set_random_seed")
-next_random_seed <- import_future("next_random_seed")
-is_valid_random_seed <- import_future("is_valid_random_seed")
-is_lecyer_cmrg_seed <- import_future("is_valid_random_seed")
-as_lecyer_cmrg_seed <- import_future("as_lecyer_cmrg_seed")
