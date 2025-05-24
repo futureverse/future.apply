ee76dc01028889941001d96a2e3b48e1091cf9dc
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-05-23 21:02:01 -0700

    future_kernapply(): tidy up + more tests

--- R/future_kernapply.R
+++ R/future_kernapply.R
@@ -3,8 +3,8 @@
 #' `future_kernapply()` is a futurized version of
 #' [stats::kernapply()], i.e. it computes, in parallel, the
 #' convolution between an input sequence and a specific kernel.
-#' Parallelization takes place when `x` is a matrix or a
-#' `tskernel` object.
+#' Parallelization takes place over columns when `x` is a matrix,
+#' including a `ts` matrix.
 #'
 #' @inheritParams stats::kernapply
 #'
@@ -15,9 +15,9 @@
 #' library(datasets)
 #' library(stats)
 #'
-#' x <- EuStockMarkets[,1]
+#' X <- EuStockMarkets[, 1:2]
 #' k <- kernel("daniell", 50)  # a long moving average
-#' x <- future_kernapply(x, k = k)
+#' X_smooth <- future_kernapply(X, k = k)
 #'
 #' @export
 future_kernapply <- function(x, ...) {
@@ -30,39 +30,23 @@ future_kernapply <- function(x, ...) {
 #' @importFrom stats kernapply
 #' @export
 future_kernapply.default <- function(x, k, circular = FALSE, ...) {
-    if (is.vector(x))
-        return (kernapply(x, k, circular=circular))
-    else if (is.matrix(x))
-        return (apply(x, MARGIN=2, FUN=future_kernapply, k, circular=circular))
-    else
-        stop ("'future_kernapply' is not available for object 'x'")
+  if (is.vector(x))
+    return(kernapply(x, k, circular = circular))
+  else if (is.matrix(x))
+    return(future_apply(x, MARGIN = 2, FUN = kernapply, k, circular = circular))
+  else
+    stop("'future_kernapply' is not available for object 'x'")
 }
+
 
 #' @rdname future_kernapply
 #'
 #' @importFrom stats kernapply end frequency ts
 #' @export
 future_kernapply.ts <- function(x, k, circular = FALSE, ...) {
-    if (!is.matrix(x))
-        y <- kernapply(as.vector(x), k, circular=circular)
-    else
-        y <- apply(x, MARGIN=2L, FUN=future_kernapply, k, circular=circular)
-    ts(y, end=end(x), frequency=frequency(x))
-}
-
-#' @rdname future_kernapply
-#'
-#' @importFrom stats kernel is.tskernel
-#' @export
-kernapply.tskernel <- function(x, k, ...) {
-    if (!is.tskernel(x))
-        stop ("'x' is not a kernel")
-    if (!is.tskernel(k))
-        stop ("'k' is not a kernel")
-    n <- k$m
-    xx <- c(rep_len(0,n), x[-x$m:x$m], rep_len(0,n))
-    coef <- future_kernapply(xx, k, circular = TRUE)
-    m <- length(coef) %/% 2L
-    kernel(coef[(m+1L):length(coef)], m,
-           paste0("Composite(", attr(x, "name"), ",", attr(k, "name"), ")"))
+  if (!is.matrix(x))
+    y <- kernapply(as.vector(x), k, circular = circular)
+  else
+    y <- future_apply(x, MARGIN = 2, FUN = kernapply, k, circular = circular)
+  ts(y, end = end(x), frequency = frequency(x))
 }
--- inst/testme/test-future_kernapply.R
+++ inst/testme/test-future_kernapply.R
@@ -1,25 +1,41 @@
 if (require("datasets") && require("stats")) {
   library(future.apply)
+  library(datasets)
   
   plan(multisession)
   
   ## Adopted from example("kernapply", package = "stats")
-  
-  x <- EuStockMarkets[,1]
-  
+
+  ## ------------------------------------------------------
+  ## Test {future_}kernapply() for 'default'
+  ## ------------------------------------------------------
+  X <- EuStockMarkets[, 1:2]
+  X <- unclass(X)
+  stopifnot(inherits(X, "matrix"), !inherits(X, "ts"))
+
   k1 <- kernel("daniell", m = 50L)
-  x1_truth <- kernapply(x, k = k1)
-  str(x1_truth)
-  x1 <- future_kernapply(x, k = k1)
-  str(x1)
-  stopifnot(identical(x1, x1_truth))
-  
-  k2 <- kernel("daniell", m = 10L)
-  x2_truth <- kernapply(x, k = k2)
-  str(x2_truth)
-  x2 <- future_kernapply(x, k = k2)
-  str(x2)
-  stopifnot(identical(x2, x2_truth))
-  
+  stopifnot(inherits(k1, "tskernel"))
+  X1_truth <- kernapply(X, k = k1)
+  str(X1_truth)
+  X1 <- future_kernapply(X, k = k1)
+  str(X1)
+  stopifnot(identical(X1, X1_truth))
+
+
+  ## ------------------------------------------------------
+  ## Test {future_}kernapply() for 'ts'
+  ## ------------------------------------------------------
+  X <- EuStockMarkets[, 1:2]
+  stopifnot(inherits(X, "matrix"), inherits(X, "ts"))
+
+  k1 <- kernel("daniell", m = 50L)
+  stopifnot(inherits(k1, "tskernel"))
+  X1_truth <- kernapply(X, k = k1)
+  str(X1_truth)
+  X1 <- future_kernapply(X, k = k1)
+  str(X1)
+  stopifnot(identical(X1, X1_truth))
+
+
   plan(sequential)
 }
